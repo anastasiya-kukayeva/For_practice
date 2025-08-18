bce635786600f347c4e0f02efce6b15aff7a285d
Anastasiya_Kukayeva <anastasiya.kukayeva@gmail.com>
2025-08-18 10:28:26 +0300

    Added successful login test login.spec.ts Changed setting of timeout in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,22 +32,24 @@ export default defineConfig({
     trace: "on-first-retry",
   },
 
+  timeout: 60_000,
+
   /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    //
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
+    //
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/login.spec.ts
@@ -0,0 +1,31 @@
+// Валидный тест с валидными кредами
+
+import { test, expect } from "@playwright/test"
+import { getCurrentStringDate } from "../utils/getCurrentStringDate"
+
+const URL = "https://test-env08-clinic.robo-med.com"
+
+test("successful_test", async ({ page }) => {
+  await page.goto(URL)
+  const logo = page.getByRole("img", { name: "logo" })
+  await expect(logo).toBeVisible()
+  await expect(page.getByRole("heading", { name: "logo Robomed Clinic" })).toBeVisible()
+  const loginField = page.getByRole("textbox", { name: "Имя пользователя" })
+  await loginField.click()
+  await loginField.fill("Support")
+  const passwordField = page.getByRole("textbox", { name: "Пароль" })
+  await passwordField.click()
+  await passwordField.fill("sup321")
+  // await passwordField.press("Enter")
+  const button = page.getByRole("button", { name: "Войти" })
+  await button.click()
+  await expect(page).toHaveURL(`${URL}`)
+  await expect(page.getByRole("heading", { name: "Загрузка справочников" })).toBeVisible()
+  // await page.waitForTimeout(10000)
+  await expect(page).toHaveURL(`${URL}/schedule/daily/${getCurrentStringDate()}`, {
+    timeout: 300_000,
+  })
+  // await expect(page).toHaveURL(URL)
+  // const dropDown = page.getByRole("listbox")
+  // await expect(dropDown).toBeVisible()
+})
