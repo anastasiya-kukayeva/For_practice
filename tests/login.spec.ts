2de39a03488cbbc4f00165e9dcbac63ea536fff8
Anastasiya_Kukayeva <anastasiya.kukayeva@gmail.com>
2025-08-18 21:19:09 +0300

    Added .env

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,19 +3,21 @@
 import { test, expect } from "@playwright/test"
 import { getCurrentStringDate } from "../utils/getCurrentStringDate"
 
-const URL = "https://test-env08-clinic.robo-med.com"
-
 test("successful_test", async ({ page }) => {
+  const username = process.env.LOGIN // your_username
+  const password = process.env.PASSWORD // your_strong_password
+  const URL = process.env.BASE_URL // your_strong_url
+
   await page.goto(URL)
   const logo = page.getByRole("img", { name: "logo" })
   await expect(logo).toBeVisible()
   await expect(page.getByRole("heading", { name: "logo Robomed Clinic" })).toBeVisible()
   const loginField = page.getByRole("textbox", { name: "Имя пользователя" })
   await loginField.click()
-  await loginField.fill("Support")
+  await loginField.fill(username)
   const passwordField = page.getByRole("textbox", { name: "Пароль" })
   await passwordField.click()
-  await passwordField.fill("sup321")
+  await passwordField.fill(password)
   const button = page.getByRole("button", { name: "Войти" })
   await button.click()
   await expect(page).toHaveURL(`${URL}`)
