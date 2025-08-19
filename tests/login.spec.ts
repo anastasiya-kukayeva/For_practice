05fa205c26e13c820e52677316bcfe1c0dbfb5e8
Anastasiya_Kukayeva <anastasiya.kukayeva@gmail.com>
2025-08-19 14:36:07 +0300

    Added steps in successful login test. Rewrote locators to XPath.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,21 +8,39 @@ test("successful_test", async ({ page }) => {
   const password = process.env.PASSWORD // your_strong_password
   const URL = process.env.BASE_URL // your_strong_url
 
-  await page.goto(URL)
-  const logo = page.getByRole("img", { name: "logo" })
-  await expect(logo).toBeVisible()
-  await expect(page.getByRole("heading", { name: "logo Robomed Clinic" })).toBeVisible()
-  const loginField = page.getByRole("textbox", { name: "Имя пользователя" })
-  await loginField.click()
-  await loginField.fill(username)
-  const passwordField = page.getByRole("textbox", { name: "Пароль" })
-  await passwordField.click()
-  await passwordField.fill(password)
-  const button = page.getByRole("button", { name: "Войти" })
-  await button.click()
-  await expect(page).toHaveURL(`${URL}`)
-  await expect(page.getByRole("heading", { name: "Загрузка справочников" })).toBeVisible()
-  await expect(page).toHaveURL(`${URL}/schedule/daily/${getCurrentStringDate()}`, {
-    timeout: 300_000,
+  await test.step("Открыть страницу", async () => {
+    await page.goto(URL)
+  })
+  await test.step("Проверить логотип", async () => {
+    const logo = page.locator('//img[@alt="logo"]')
+    await expect(logo).toBeVisible()
+  })
+  await test.step("Проверить заголовок", async () => {
+    await expect(page.locator('//h2[@class="_title_1ptgq_1"]')).toBeVisible()
+  })
+  await test.step("Проверить имя пользователя", async () => {
+    const loginField = page.locator('//input[@type="text"]')
+    await loginField.click()
+    await loginField.fill(username)
+  })
+  await test.step("Проверить пароль", async () => {
+    const passwordField = page.locator('//input[@type="password"]')
+    await passwordField.click()
+    await passwordField.fill(password)
+  })
+  await test.step('Клик по кнопке "Войти"', async () => {
+    const button = page.locator('//button[@type="submit"]')
+    await button.click()
+  })
+  await test.step("Проверить адрес страницы", async () => {
+    await expect(page).toHaveURL(`${URL}`)
+  })
+  await test.step("Проверить заголовок на следующе странице", async () => {
+    await expect(page.locator('//*[contains(text(), "Загрузка справочников")]')).toBeVisible()
+  })
+  await test.step("Проверить адрес сстраницы", async () => {
+    await expect(page).toHaveURL(`${URL}/schedule/daily/${getCurrentStringDate()}`, {
+      timeout: 300_000,
+    })
   })
 })
